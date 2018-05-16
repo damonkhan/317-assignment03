ec1a8731f2dc46e7d6462b5ef54596d183710ced
damonkhan <damonkhan92@gmail.com>
2018-05-16 12:58:03 +1200

    implemented parser shown in lectures

--- src/com/company/REcompile.java
+++ src/com/company/REcompile.java
@@ -2,7 +2,56 @@ package com.company;
 
 public class REcompile {
 
+    private static String p;
+    private static int j;
+
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+
+        p = "hello";
+    }
+
+   private static void expression() {
+        term();
+        if(isVocab(p.charAt(j)) || p.charAt(j) == '(')
+            expression();
+    }
+
+    private static void term() {
+        factor();
+        if(p.charAt(j) == '*')
+            j++;
+        if(p.charAt(j) == '+') {
+            j++;
+            term();
+        }
+    }
+
+    private static void factor() {
+        if(isVocab(p.charAt(j)))
+            j++;
+        else {
+            if (p.charAt(j) == '(') {
+                j++;
+                expression();
+                if (p.charAt(j) == ')')
+                    j++;
+                else
+                    System.err.println("Unbalanced brackets - not W.F. expression.");
+            }
+            else {
+                System.err.println("Not W.F. expression.");
+            }
+        }
+    }
+
+    private static void parse() {
+        expression();
+        if (p.charAt(j) != '\"')
+            System.err.println("Error - expression not part of language.");
+    }
+
+    private static boolean isVocab(char element) {
+        return true;
     }
 }
+
---
+++ src/com/company/Test.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Test {
+     public static void main(String[] args) {
+         String p = "hello";
+         int j = 0;
+
+         if (p.charAt(j) == 'h') {
+             System.out.println("yes");
+         }
+     }
+
+}
