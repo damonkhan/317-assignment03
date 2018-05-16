e1f8c891b9b0e27197738e8c99593f38b9f335eb
damonkhan <damonkhan92@gmail.com>
2018-05-16 14:43:17 +1200

    Begin converting parser to compiler

--- src/com/company/REcompile.java
+++ src/com/company/REcompile.java
@@ -1,17 +1,25 @@
 package com.company;
 
+import com.sun.xml.internal.bind.v2.TODO;
+
 import java.text.ParseException;
 
 public class REcompile {
 
     private static String p;
     private static int j;
+    private static char[] c;
+    private static int[] n1;
+    private static int[] n2;
+    private static int currState; // the current state being built
 
     public static void main(String[] args) {
 
         try {
+
             j = 0;
-            p = "\"a**bc(a+b)d\"";
+            p = "\"a*bc(a+b)d\"";
+            initialise(); // initialise arrays and state
             parse();
 
         } catch (ParseException ex) {
@@ -19,7 +27,7 @@ public class REcompile {
         }
     }
 
-   private static void expression() throws ParseException {
+    private static void expression() throws ParseException {
         term();
         if(isVocab(p.charAt(j)) || p.charAt(j) == '(')
             expression();
@@ -73,6 +81,27 @@ public class REcompile {
         }
     }
 
-    private static boolean isVocab(char element) { return element != '(' && element != ')' && element != '*' && element != '+' && element != '\"'; }
+    private static boolean isVocab(char element) {
+        return element != '(' && element != ')' && element != '*' && element != '+' && element != '\"';
+    }
+
+    private static int setState(int s, char ch, int next1, int next2) {
+       c[s] = ch;
+       n1[s] = next1;
+       n2[s] = next2;
+
+       return currState = s + 1;
+    }
+
+    /*
+    TODO: find way to make this method dynamic
+    TODO: or let size arg be passed to method.
+     */
+    private static void initialise() {
+        c = new char[100];
+        n1 = new int[100];
+        n2 = new int[100];
+        currState = 0;
+    }
 }
 
