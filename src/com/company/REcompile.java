a48c1e09f35b9796b0a7fd11e036f56754a4d582
Damon Khan <support@codeavengers.com>
2018-05-22 00:10:18 +1200

    + operator should be working now

--- src/com/company/REcompile.java
+++ src/com/company/REcompile.java
@@ -12,7 +12,6 @@ public class REcompile {
     private static ArrayList<Integer> n2;
     private static int state; // the next state to be built
 
-    //TODO: 4. implement + operator
     //TODO: 5. implement ? operator
     //TODO: 6. implement escaped operators '\'
     //TODO: 7. abstract out operator rules into seperate methods
